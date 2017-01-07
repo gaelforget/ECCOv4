3dc53c4f0b3eeb2e726abd1cbac3c76a6150a1be
gaelforget <gforget@mit.edu>
2017-01-06 22:24:41 -0500

    add updates and tests

--- example_scripts/setup_rhel.sh
+++ example_scripts/setup_rhel.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#apply system updates
+sudo yum -y update
+
 #install compiler and other tools
 sudo yum -y install cvs
 sudo yum -y install git
@@ -12,17 +15,21 @@ sudo yum -y install gcc-gfortran
 sudo yum -y install openmpi-devel
 
 #install netcdf via Extra Packages for Enterprise Linux (EPEL)
+if [ ! -e epel-release-6-8.noarch.rpm ]; then
 wget https://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+else
+sleep 10
+fi
 sudo rpm -i epel-release-6-8.noarch.rpm
 sudo yum -y install netcdf-devel
 
 #define environment variables needed to run MITgcm
+if [ ! -e setup_export.sh ]; then
 {
   echo '#!/bin/bash'
   echo 'export PATH=$PATH:/usr/lib64/openmpi/bin'
   echo 'export LD_LIBRARY_PATH=/usr/lib64/openmpi/lib'
   echo 'export MPI_INC_DIR=/usr/include/openmpi-x86_64'
-  echo 'export GFTEST=gftest'
 } > setup_export.sh
-#source ./setup_export.sh
+fi
 
