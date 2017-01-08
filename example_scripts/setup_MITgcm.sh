3c6f73369cd12656c5775bd5de2056e55df2bf4c
gaelforget <gforget@mit.edu>
2017-01-08 16:56:21 -0500

    couple tweaks
    1) input_init will be obtained from inputs_baseline2 so no need to
    download it; 2) change back to root directory after having compiled

--- example_scripts/setup_MITgcm.sh
+++ example_scripts/setup_MITgcm.sh
@@ -12,8 +12,6 @@ mkdir MITgcm/mysetups
 cd MITgcm/mysetups
 git clone https://github.com/gaelforget/ECCO_v4_r2
 mkdir ECCO_v4_r2/run
-wget --recursive ftp://mit.ecco-group.org/ecco_for_las/version_4/release2/input_init/
-mv mit.ecco-group.org/ecco_for_las/version_4/release2/input_init ECCO_v4_r2/input_fields
 cd ../..
 
 #compile and run short MITgcm test
@@ -26,6 +24,7 @@ cd MITgcm/mysetups/ECCO_v4_r2/build
 ../../../tools/genmake2 -mods=../code -optfile ../../../tools/build_options/linux_amd64_gfortran -mpi
 make depend
 make -j 4
+cd ~
 
 fi
 
