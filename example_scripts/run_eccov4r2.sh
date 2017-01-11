ebfd926c22bf2163fa36704f31f4094841da62b0
gaelforget <gforget@mit.edu>
2017-01-11 14:18:18 -0500

    edit paths and rename scripts

--- example_scripts/run_eccov4r2.sh
+++ example_scripts/run_eccov4r2.sh
@@ -4,11 +4,11 @@
 #export LD_LIBRARY_PATH=/usr/lib64/openmpi/lib
 #export MPI_INC_DIR=/usr/include/openmpi-x86_64
 
+cd /shared
 source ./setup_export.sh
 
-mkdir run
-
-cd run
+mkdir /shared/run
+cd /shared/run
 ln -s ../MITgcm/mysetups/ECCO_v4_r2/build/mitgcmuv .
 ln -s ../MITgcm/mysetups/ECCO_v4_r2/input/* .
 ln -s ../inputs_baseline2/input*/* .
--- example_scripts/run_test.sh
+++ example_scripts/run_test.sh
@@ -4,8 +4,9 @@
 #export LD_LIBRARY_PATH=/usr/lib64/openmpi/lib
 #export MPI_INC_DIR=/usr/include/openmpi-x86_64
 
+cd /shared
 source ./setup_export.sh
 
-cd /home/ec2-user/MITgcm/verification/ideal_2D_oce/run
+cd /shared/MITgcm/verification/ideal_2D_oce/run
 mpiexec -np 2 ./mitgcmuv
 
