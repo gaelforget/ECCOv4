5968a9ea0eb4a0da11f257d22e29e8abfeed4674
gaelforget <gforget@mit.edu>
2017-01-25 21:22:17 -0500

    execute setup_rhel.sh and setup_export.sh within setup_MITgcm.sh

--- example_scripts/setup_MITgcm.sh
+++ example_scripts/setup_MITgcm.sh
@@ -2,6 +2,12 @@
 
 if [ ! -e MITgcm ]; then
 
+#update software if needed and generate setup_export.sh
+source ./ECCO_v4_r2/example_scripts/setup_rhel.sh
+
+#define environment variables that MITgcm relies on
+source ./setup_export.sh
+
 #install MITgcm
 echo '/1 :pserver:cvsanon@mitgcm.org:2401/u/gcmpack Ah<Zy=0=' > ~/.cvspass
 export CVS_RSH=ssh
@@ -27,7 +33,10 @@ make -j 4
 cd ../../../..
 
 #display license
+echo " "
+echo " "
 cat MITgcm/LICENSE.txt
+echo " "
 
 fi
 
